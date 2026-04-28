c46286a5e97afef33b5d1f1a2b184e1a19c7bb72
kwunderle <katharinewunderle@gmail.com>
2026-04-27 21:24:03 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv4TJvS5zTlPlN3myiWPN1HP3S3cGMFNs",
+  authDomain: "crimechester-a1ff4.firebaseapp.com",
+  projectId: "crimechester-a1ff4",
+  storageBucket: "crimechester-a1ff4.firebasestorage.app",
+  messagingSenderId: "911147423532",
+  appId: "1:911147423532:web:661a7d0f69757fe3843e89"
+};
+
+const app = initializeApp(firebaseConfig);
